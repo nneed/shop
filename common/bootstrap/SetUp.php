724e0f7bbcd0cf7285aca6d8ca4efc2a02edb224
Kazankov <nukazankov@rus-telecom.ru>
2018-08-14 17:17:26 +0300

    Создание файла bootstrap

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: НЮКазанков
+ * Date: 14.08.2018
+ * Time: 17:06
+ */
+
+namespace common\bootstrap;
+
+
+use yii\base\BootstrapInterface;
+
+class SetUp implements BootstrapInterface
+{
+    public function bootstrap($app) : void
+    {
+
+    }
+}
--- common/config/main.php
+++ common/config/main.php
@@ -5,10 +5,11 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'bootstrap' => 'common\bootstrap\SetUP',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
-            'cachePath' => '@common/runtime/cache',
+            'class' => 'yii\caching\MemCache',
+            'useMemcached' => true
         ],
     ],
 ];

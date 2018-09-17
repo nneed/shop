d5a5e669a5712133ac295e37d2041467a31b563f
Kazankov <nukazankov@rus-telecom.ru>
2018-09-17 14:00:52 +0300

    Исправляем конфиги для тестов

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,7 +9,10 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => [
+        'log',
+        'bootstrap' => 'common\bootstrap\SetUp',
+    ],
     'aliases' => [
         '@staticRoot' => $params['staticPath'],
         '@static'   => $params['staticHostInfo'],

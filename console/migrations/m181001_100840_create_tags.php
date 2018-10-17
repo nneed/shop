a034195556ae3b236020ccdd0f412c00f0305142
Kazankov <nukazankov@rus-telecom.ru>
2018-10-17 17:06:18 +0300

    Добавил миграцию, сервис, форму и репозиторий для контроллера

--- console/migrations/m181001_100840_create_tags.php
+++ console/migrations/m181001_100840_create_tags.php
@@ -10,19 +10,20 @@ class m181001_100840_create_tags extends Migration
     /**
      * {@inheritdoc}
      */
-    public function safeUp()
+    public function up()
     {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
         $this->createTable('{{%shop_tags}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull(),
             'slug' => $this->string()->notNull(),
-        ]);
+        ], $tableOptions);
+
+        $this->createIndex('{{%idx-shop_tags-slug}}', '{{%shop_tags}}', 'slug', true);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
+    public function down()
     {
         $this->dropTable('{{%shop_tags}}');
     }

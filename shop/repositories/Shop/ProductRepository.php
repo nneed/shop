460db8154ed1efd430f972a56759c45e74fb3691
Kazankov <nukazankov@rus-telecom.ru>
2019-02-06 10:36:32 +0300

    Очищение кэша

--- shop/repositories/Shop/ProductRepository.php
+++ shop/repositories/Shop/ProductRepository.php
@@ -2,11 +2,21 @@
 
 namespace shop\repositories\Shop;
 
+use shop\dispatchers\EventDispatcher;
 use shop\entities\Shop\Product\Product;
+use shop\repositories\events\EntityPersisted;
+use shop\repositories\events\EntityRemoved;
 use shop\repositories\NotFoundException;
 
 class ProductRepository
 {
+    private $dispatcher;
+
+    public function __construct(EventDispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
     public function get($id): Product
     {
         if (!$product = Product::findOne($id)) {
@@ -30,6 +40,8 @@ class ProductRepository
         if (!$product->save()) {
             throw new \RuntimeException('Saving error.');
         }
+        $this->dispatcher->dispatchAll($product->releaseEvents());
+        $this->dispatcher->dispatch(new EntityPersisted($product));
     }
 
     public function remove(Product $product): void
@@ -37,6 +49,7 @@ class ProductRepository
         if (!$product->delete()) {
             throw new \RuntimeException('Removing error.');
         }
-
+        $this->dispatcher->dispatchAll($product->releaseEvents());
+        $this->dispatcher->dispatch(new EntityRemoved($product));
     }
 }
